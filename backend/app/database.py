c2ed80701d757d833f6181ab2fcde81aa5dce30d
Santana <viniciusantana76@gmail.com>
2026-06-17 01:16:05 -0300

    fix: torna a DATABASE_URL dinamica pra reconhecer o render

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,15 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
+from dotenv import load_dotenv
 
+#carregar variaveis de ambient
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:Wandinha#02@db.asizpxfmfyztlsiaesth.supabase.co:5432/postgres")
-
+# Puxa a url do render
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 """
@@ -26,4 +27,5 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 postgresql://postgres:Wandinha#02@db.asizpxfmfyztlsiaesth.supabase.co:5432/postgres
+postgresql://postgres.asizpxfmfyztlsiaesth:Wandinha#02@aws-1-sa-east-1.pooler.supabase.com:5432/postgres
 """
